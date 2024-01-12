1c0ff7aec1adede847ef9ddd15f05e22fc1eea70
Sergey Zwezdin <sergey@zwezdin.com>
2024-01-12 18:36:53 +0300

    feat: Migrate to rollup.js

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import del from 'rollup-plugin-delete'
+import { dts } from 'rollup-plugin-dts'
+import external from 'rollup-plugin-peer-deps-external'
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'lib/index.js',
+                format: 'cjs',
+                name: 'lib',
+            },
+        ],
+        plugins: [del({ targets: 'lib/*' }), external(), resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+    },
+    {
+        input: 'lib/types/index.d.ts',
+        output: [
+            {
+                file: 'lib/index.d.ts',
+                format: 'cjs',
+                name: 'types'
+            },
+        ],
+        plugins: [dts(), del({ targets: 'lib/types', hook: 'buildEnd' })],
+    },
+]
